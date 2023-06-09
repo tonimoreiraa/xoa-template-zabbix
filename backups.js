fa84415678758645eba95c6f83d2e02aa02a0d10
Toni Moreira <toni@itentecnologia.com.br>
2023-06-09 15:08:27 -0300

    Add backup module

---
+++ backups.js
@@ -0,0 +1,19 @@
+import { connectXo } from './src/connect.js';
+
+const xo = await connectXo()
+
+const response = await xo.call('backupNg.getAllLogs')
+var jobIDs = Object.values(response).map(obj => obj.jobId)
+jobIDs = jobIDs.filter((v, i) => jobIDs.indexOf(v) == i)
+
+const jobs = Object.fromEntries(jobIDs.map(jobId => [jobId, Object.values(response).filter(x => x.jobId == jobId).sort((x, y) => (x.end - y.end)).reverse()[0]]))
+
+const command = process.argv[6]
+if (command == 'get-all') {
+  console.log(JSON.stringify({data: Object.keys(jobs).map(jobId => ({"#{BACKUP_ID}": jobId}))}))
+} else if (command == 'get') {
+  const [id, param] = process.argv.slice(7)
+  console.log(jobs[id][param])
+}
+
+xo.close()
--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
+echo "Installing Node Packages"
+npm install
+
+echo "Linking zabbix to script"
+
 ln -s $(pwd)/index.js /usr/lib/zabbix/externalscripts/xoa.command
